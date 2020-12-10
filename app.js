4b259fd76c58668ca58a70d56a12b93e08810b9b
Xparham <xparham@gmail.com>
2020-12-10 13:20:47 -0500

    added email in event listener and error fields

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const regForm = document.getElementById('registration-form')
 const username = document.getElementById('username')
 const usernameErr = username.nextElementSibling
+
+
+const email = document.getElementById('email')
+const emailErr = email.nextElementSibling
 // const usernameErr = document.getElementById("usernameErr")
 
 // 2. Add event / modification
@@ -17,8 +21,13 @@ regForm.addEventListener('click', function (e) {
     console.log('sending to server')
   }
 
-  //@TODO: add the validation for email
-
+  if (email.value === '') {
+    emailErr.className = 'block bg-red-500 text-white'
+  } else {
+    console.log('sending to server')
+  }
+  
   //@TODO: add the validation for password
 
 })
+
