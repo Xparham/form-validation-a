aad8ef2753e265b97531a94525b75cec0fcba8c4
Xparham <xparham@gmail.com>
2020-12-14 16:32:20 -0500

    Added username, email, password, and password 2 Element ID

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const regForm = document.getElementById('registration-form')
 
 const username = document.getElementById('username')
 const email = document.getElementById('email')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
 
 regForm.addEventListener('submit', function (e) {
   e.preventDefault()
 
   validateEmpty(username)
+  validateIsEmail(email)
+  validatePassMatch(password)
+  validateEmpty(password2)
 
   //@TODO: add the validation for email (Code Challenge 5a)
   //@TODO: Code Challenge 5b: Refactor your CC 5a to use function with the "blueprints" below
@@ -25,22 +30,29 @@ function validateEmpty(input) {
   }
 }
 
-function showError(input){
-  // steps to do this...
-  console.log('input is empty')
+
+function validateIsEmail(input){
+  //@TODO: check if input is an email
+  console.log(input)
+  
+
+
 }
 
-function showSuccess () {
-    console.log('you are ready to submit')
+function showError(input){
+  // steps to do this...
+input.nextElementSibling.innerHTML = '<small class="error"> ❌ Please enter your username </small>'
+  
+}
+
+function showSuccess (input) {
+input.nextElementSibling.innerHTML = '<small class="success"> ✔️ Success </small>'
 }
 
 function validatePassMatch(password1, password2){
   //@TODO: check if the passwords match
 }
 
-function validateIsEmail(email){
-  //@TODO: check if input is an email
-}
 
 
 function validateMinLength(input){
